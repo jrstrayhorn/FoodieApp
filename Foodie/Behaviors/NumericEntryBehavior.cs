4fe08da2665972b111f27c612aa152b38b9f49b0
jrstrayhorn <jrstrayhorn@gmail.com>
2018-03-22 11:18:59 -0400

    Finished Extending Control Functionality

---
+++ Foodie/Behaviors/NumericEntryBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Foodie.Behaviors
+{
+    public class NumericEntryBehavior : Behavior<Entry>
+    {
+		protected override void OnAttachedTo(Entry bindable)
+		{
+			base.OnAttachedTo(bindable);
+            bindable.TextChanged += TextChanged_Handler;
+		}
+
+        void TextChanged_Handler(object sender, TextChangedEventArgs e)
+        {
+            // short circuit for no value
+            if (string.IsNullOrEmpty(e.NewTextValue))
+                return;
+
+            double _;
+            if (!double.TryParse(e.NewTextValue, out _))
+                ((Entry)sender).Text = e.OldTextValue;
+        }
+
+		protected override void OnDetachingFrom(Entry bindable)
+		{
+			base.OnDetachingFrom(bindable);
+            bindable.TextChanged -= TextChanged_Handler;
+		}
+
+	}
+}
